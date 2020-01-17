63a1564a508216bb95594f1622e136fe8ee18806
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 11:23:42 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+
+def map(array)
+  new_array = []
+  array.each { |element|
+    new_array << yield( element )
+    }
+  new_array
+end
+
+def reduce (array, sv=nil)
+
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.size
+    sum = yield(sum, array[i])
+    i+= 1
+  end
+  sum
+end
